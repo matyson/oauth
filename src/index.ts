6fc6fa2830ea36f82b4fc9d157309c2bd2bd998e
Matheus Luís <matheus.bernardi@lnls.br>
2025-02-07 13:43:41 -0300

    add KeycloakPayload type definition and logging for access token

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,38 @@ import { subjects } from "./subjects";
 import { KeycloakProvider } from "@openauthjs/openauth/provider/keycloak";
 import { env } from "./env";
 
+console.log(env);
+
+type KeycloakPayload = {
+  exp: number;
+  iat: number;
+  auth_time: number;
+  jti: string;
+  iss: string;
+  aud: string;
+  sub: string;
+  typ: string;
+  azp: string;
+  sid: string;
+  acr: string;
+  "allowed-origins": string[];
+  realm_access: {
+    roles: string[];
+  };
+  resource_access: {
+    account: {
+      roles: string[];
+    };
+  };
+  scope: string;
+  email_verified: boolean;
+  name: string;
+  preferred_username: string;
+  given_name: string;
+  family_name: string;
+  email: string;
+};
+
 export default issuer({
   subjects,
   storage: MemoryStorage(),
@@ -18,10 +50,12 @@ export default issuer({
   },
   success: async (ctx, value) => {
     if (value.provider === "keycloak") {
+      console.log(value.tokenset.access);
       const jwt = value.tokenset.access;
       const payload = jwt.split(".")[1];
       const decoded = Buffer.from(payload, "base64").toString("utf-8");
-      const json = JSON.parse(decoded);
+      const json = JSON.parse(decoded) as KeycloakPayload;
+      console.log(json);
       return ctx.subject("user", {
         id: json.sub,
         email: json.email,
